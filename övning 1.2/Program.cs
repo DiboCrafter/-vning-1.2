013b96a0439d84682d19b05521fb06406dcf9e06
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-13 12:16:53 +0100

    Add project files.

---
+++ övning 1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Övning1_2
+{
+    class Program
+    {
+     static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("ERROR");
+        }
+
+    }
+}
